c46de550b034f617c9d084b9c06b7b7eab494b93
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-07-07 23:59:06 +0530

    refector : footer code updated

--- my-portfolio/src/components/footer/Footer.jsx
+++ my-portfolio/src/components/footer/Footer.jsx
@@ -1,35 +1,115 @@
 import React from "react";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
-import {SiLeetcode} from "react-icons/si";
+import { socialsData } from "../contact/SocialData";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  FaTwitter,
+  FaLinkedinIn,
+  FaGithub,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
+import {SiHashnode} from "react-icons/si"
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: "#152b61 !important",
+    color: "white",
+    height: "fit-content",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    gap: "2rem",
+       boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.6)",
+    padding: "2rem 0",
+  },
+  socialmedia: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "1rem",
+  },
+
+  socialIcon: {
+    margin: "0 0.5rem",
+    fontSize: "1.5rem",
+    transition: "all 0.2s ease-in-out",
+    "&:hover": {
+      transform: "scale(1.2)",
+    },
+  },
+}));
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <footer class="p-4 bg-skyblue rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 footer">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 footer-left-text">
-        Developed by Abhinav Kumar
-      </span>
-      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 footer-contact">
-        <li>
-          <a href="https://github.com/Abhinav2011" class="mr-4 hover:underline md:mr-6 ">
-            <BsGithub style={{color:'#87CEEB',width:"200%",height:"auto"}}/>
+    <footer className={classes.footer}>
+      <div className={classes.socialmedia}>
+        {socialsData.whatsapp && (
+          <a
+            href={socialsData.whatsapp}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <FaWhatsapp aria-label="WhatsApp" style={{ color: "#25D366" }} />
           </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/abhinav-kumar-904254195/" class="mr-4 hover:underline md:mr-6">
-            <BsLinkedin style={{color:'#87CEEB',width:"200%",height:"auto"}}/>
+        )}
+        {socialsData.twitter && (
+          <a
+            href={socialsData.twitter}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <FaTwitter aria-label="Twitter" style={{ color: "#00acee " }} />
           </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/Abhinav36650041" class="mr-4 hover:underline md:mr-6">
-            <BsTwitter style={{color:'#87CEEB',width:"200%",height:"auto"}}/>
+        )}
+        {socialsData.github && (
+          <a
+            href={socialsData.github}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <FaGithub aria-label="GitHub" style={{ color: "##1d1a1a" }} />
           </a>
-        </li>
-        <li>
-          <a href="https://leetcode.com/abhi_nav2011/" class="mr-4 hover:underline md:mr-6">
-            <SiLeetcode style={{color:'#87CEEB',width:"200%",height:"auto"}}/>
+        )}
+        {socialsData.linkedIn && (
+          <a
+            href={socialsData.linkedIn}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <FaLinkedinIn aria-label="LinkedIn" style={{ color: "#0C63BC" }} />
           </a>
-        </li>
-      </ul>
+        )}
+        {socialsData.instagram && (
+          <a
+            href={socialsData.instagram}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <FaInstagram aria-label="Instagram" style={{ color: "#d62976 " }} />
+          </a>
+        )}
+        {socialsData.medium && (
+          <a
+            href={socialsData.medium}
+            target="_blank"
+            rel="noreferrer"
+            className={classes.socialIcon}
+          >
+            <SiHashnode aria-label="hashnode" style={{ color: "#2962FF" }} />
+          </a>
+        )}
+      </div>
+      <div
+        className={`${classes.footerText} text-sm text-gray-500 sm:text-center dark:text-gray-400 footer-left-text`}
+      >
+        Developed by Devesh Mehra
+      </div>
     </footer>
   );
 };
