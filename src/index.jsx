a5992668548c5c6be1b15de95e5af0056c62434d
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-08-23 11:15:25 +0530

    updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Import ReactDOM correctly
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import { inject } from '@vercel/analytics';
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
 inject();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <BrowserRouter>
+// Use ReactDOM.render instead of ReactDOM.createRoot
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-    </BrowserRouter>  
-  </>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
 
 reportWebVitals();
